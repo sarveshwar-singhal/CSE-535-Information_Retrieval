db510bf3b67e2ffd0ff7988b1884105c639fc440
base <scpl.singhal@gmail.com>
2021-12-04 18:17:07 -0500

    adding project 4

---
+++ project4/froshim0/application.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    # return "Hello World"
+    name = request.args.get("name", "world")
+    return render_template("index.html", name=name)
+
+@app.route("/register", methods=["POST"])
+def register():
+    #name = request.args.get("name") #forms sends through get are fetched using .args
+    #dorm = request.args.get("dorm") #forms send through post are fetched using .forms.get
+    name = request.form.get("name")
+    dorm = request.form.get('dorm')
+    if not name or not dorm:
+        return render_template("failure.html")
+    else:
+        return render_template("success.html")
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ project4/froshim2/application.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, request, redirect
+import os
+import smtplib
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+
+@app.route('/register', methods=["POST"])
+def register():
+    name = request.form.get("name")
+    email = request.form.get("email")
+    email = 'scpl.singhal@gmail.com'
+    dorm = request.form.get("dorm")
+    if not name or not dorm or not email:
+        return render_template("failure.html")
+    message = "you're registered"
+    server = smtplib.SMTP("smtp.gmail.com", 587)
+    server.starttls()
+    server.login('sgi.singhal@gmail.com', os.getenv("PASSWORD"))
+    server.sendmail('sgi.singhal@gmail.com', email, message)
+    return render_template('success.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
